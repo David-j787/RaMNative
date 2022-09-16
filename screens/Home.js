6dc0e30dbde25476c7ab4e63df500a3c9d1ce7cc
David <davidpatejo@gmail.com>
2022-09-16 13:31:58 -0300

    infinite scroll and some styles

--- screens/Home.js
+++ screens/Home.js
@@ -4,13 +4,18 @@ import { Text, View, FlatList, Image, StyleSheet } from 'react-native';
 
 export default function Home(){
 
-    const [characters, setCharacters] = useState(null);
+    const [characters, setCharacters] = useState([]);
+    const [page, setPage] = useState(1);
 
     const getCharacters = async () => {
-        const characters = await fetch("https://rickandmortyapi.com/api/character");
-        const a = await characters.json();
-        setCharacters(a.results);
+        const characters = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`);
+        const json = await characters.json();
+        if(json.results){
+            setCharacters(prev => [...prev, ...json.results]);
+            setPage(prev => prev + 1);
+        }else return;
     }
+
 
     useEffect(() => {
         getCharacters();
@@ -18,23 +23,32 @@ export default function Home(){
 
     const styles = StyleSheet.create({
         container: {
+            flex: 1,
             
+        },
+        cardContainer: {
+            width: "33%",
+            margin: 1
         },
         name: {
         },
         image: {
-            width: 200,
-            height: 200
+            width:  "100%",
+            height: 130
         }
     })
     
     return(
         <FlatList
+        
+        numColumns={3}
         style={styles.container}
         data={characters}
+        onEndReached={getCharacters}
+        onEndReachedThreshold={0.01}
         renderItem={({item: c}) => (
-            <View key={c.id}>
-                <Text style={styles.name}>{c.name}</Text>
+            <View style={styles.cardContainer} key={c.id}>
+                
                 <Image
                 style={styles.image}
                 source={{uri: c.image}}
