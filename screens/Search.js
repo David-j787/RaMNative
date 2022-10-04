02f4a515701776fa048326e11e124f3419a39062
David <davidpatejo@gmail.com>
2022-10-04 09:28:34 -0300

    searchBar

--- screens/Search.js
+++ screens/Search.js
@@ -1,5 +1,13 @@
 import React, {useState, useEffect} from "react";
-import { View, Text, StyleSheet, FlatList, StatusBar} from "react-native";
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    FlatList, 
+    StatusBar, 
+    TouchableWithoutFeedback,
+    Image
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 
@@ -7,8 +15,23 @@ export default function Search(){
 
     const navigation = useNavigation();
 
+    const [data, setData] = useState([]);
+
+    const setSearch = async (text) => {
+        setTimeout(async () =>{
+            if(text){
+                const search = await fetch(`https://rickandmortyapi.com/api/character/?name=${text}`);
+                const json = await search.json();
+                setData([...json.results]);
+            }else{
+                setData([]);
+            }
+
+        }, 750)
+    }
 
     useEffect(() => {
+        setSearch();
         navigation.setOptions({
             headerTitle: "",
             headerSearchBarOptions: {
@@ -16,23 +39,39 @@ export default function Search(){
                 textColor: "white",
                 hintTextColor: "white",
                 headerIconColor: "white",
-                autoFocus: true
+                autoFocus: true,
+                onChangeText: (e) => setSearch(e.nativeEvent.text)
             },
         })
     }, [navigation])
 
     const styles = StyleSheet.create({
-
         container: {
             backgroundColor: "#100F0F",
             height: "100%"
+        },
+        cardImage: {
+            width:  130,
+            height: 130
         }
     })
 
     return(
         <View style={styles.container}>
             <StatusBar/>
-            <Text>o</Text>
+            <FlatList
+            numColumns={3}
+            data={data}
+            renderItem={({item: c}) => (
+                <TouchableWithoutFeedback>
+
+                    <Image
+                    style={styles.cardImage}
+                    source={{uri: c.image}}
+                    />
+                </TouchableWithoutFeedback>
+            )}
+            />
         </View>
     )
 }
